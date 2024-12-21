e77e9385117cbafa7f1e40f1aa3f0e02050f5873
lucaspeterc <lucasciolkowski@gmail.com>
2024-12-21 18:59:00 +0100

    n

--- app/components/logo.jsx
+++ app/components/logo.jsx
@@ -8,7 +8,7 @@ export default function Logo() {
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           <img
               alt="Roma"
-              src="/assets/Roma.png"
+              src="/assets/roma.png"
               width={158}
               height={48}
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
